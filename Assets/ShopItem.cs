d3c58954f393436e9855d34088f94b312f4f5cb1
Justin Whitfield <91854905+justinwhitfieldd@users.noreply.github.com>
2024-04-05 00:45:21 -0500

    proj setup, shop system, explosive system, some game logic

---
+++ Assets/ShopItem.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopItemDragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject icon;
+    public GameObject PurchaseItem;
+    public Transform BombParentObject;
+    public float ItemScale = 0.5f;
+    public GameObject PriceCounter;
+        
+    private TMPro.TextMeshProUGUI priceCounterNumber;
+    private GameManager gameManager;
+    private int PurchaseItemPrice;
+    private GameObject currentDraggedItem;
+    private GameObject instancedItem;
+    private Vector3 startPosition;
+    private Camera mainCamera;
+    private bombScript bombScript;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        bombScript = PurchaseItem.GetComponent<bombScript>();
+        PurchaseItemPrice = bombScript.price;
+        priceCounterNumber = PriceCounter.GetComponent<TMPro.TextMeshProUGUI>();
+        priceCounterNumber.text = PurchaseItemPrice.ToString();
+        gameManager = GetComponentInParent<GameManager>();
+        // Get the sprite from the PurchaseItem
+        SpriteRenderer spriteRenderer = PurchaseItem.GetComponent<SpriteRenderer>();
+        
+        if (spriteRenderer != null)
+        {
+            // Set the image displayed on the icon to the sprite from the PurchaseItem
+            Image image = icon.GetComponent<Image>();
+            if (image != null)
+            {
+                image.sprite = spriteRenderer.sprite;
+            }
+        }
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+
+        // Create a new instance of the prefab
+        instancedItem = Instantiate(PurchaseItem, GetMouseWorldPosition(), Quaternion.identity);
+        instancedItem.transform.localScale = Vector3.one * ItemScale;
+
+        // Get the sprite from the PurchaseItem
+        SpriteRenderer spriteRenderer = PurchaseItem.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            // Set the image displayed on the icon to the sprite from the PurchaseItem
+            Image image = icon.GetComponent<Image>();
+            if (image != null)
+            {
+                image.sprite = spriteRenderer.sprite;
+            }
+        }
+
+        // Set the dragged item to the new instance
+        currentDraggedItem = instancedItem;
+
+        // Set the parent of the dragged item to a different game object in the scene
+        currentDraggedItem.transform.SetParent(BombParentObject, false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (currentDraggedItem != null)
+        {
+            currentDraggedItem.transform.position = GetMouseWorldPosition();
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (currentDraggedItem != null)
+        {
+            if (CanPurchaseItem())
+            {
+                // Perform the purchase logic here
+                Debug.Log("Item purchased: " + PurchaseItem.name + " for $"+PurchaseItemPrice);
+                gameManager.budget -= PurchaseItemPrice;
+                currentDraggedItem.transform.SetParent(BombParentObject, false);
+            }
+            else
+            {
+                // Return the item to its original position if the purchase is not made
+                currentDraggedItem.transform.position = startPosition;
+                Destroy(currentDraggedItem);
+            }
+        }
+    }
+
+    private bool CanPurchaseItem()
+    {
+        if(PurchaseItemPrice <= gameManager.budget)
+            return true;
+        else
+            return false;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f; // Adjust the Z value based on your camera's near clip plane
+        return mainCamera.ScreenToWorldPoint(mousePos);
+    }
+}
---
+++ Assets/StructureManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureManager : MonoBehaviour
+{
+   public bool shouldExplode = false;
+}
