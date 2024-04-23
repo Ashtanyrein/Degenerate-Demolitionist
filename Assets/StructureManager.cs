605da044a8df85c75447c7231950231fc67ef080
mtswip <92600965+mtswip@users.noreply.github.com>
2024-04-23 11:29:23 -0500

    Added variables to start work on win/loss state

--- Assets/StructureManager.cs
+++ Assets/StructureManager.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class StructureManager : MonoBehaviour
 {
-   public bool shouldExplode = false;
+    public float height = 0.0f;
+    public bool shouldExplode = false;
+    public List<GameObject> objects = new List<GameObject>();
+    private void Start()
+    {
+        //need to fill objects with all structure components and set height
+    }
+    void Update()
+    {
+        //update object positions and height
+    }
 }
+
